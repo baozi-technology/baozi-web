345b5c8cd740b824ef813a08cee9efb8b1ed20bb
NicoGim <n.gimenez66@gmail.com>
2019-12-22 13:09:28 +0100

    Comma like in example...

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
           {
             resolve: "gatsby-remark-images",
             options: {
-              maxWidth: 700
+              maxWidth: 700,
             }
           },
           {
