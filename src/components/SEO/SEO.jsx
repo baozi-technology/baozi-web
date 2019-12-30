3d1fb96d202979e8927bec05760d6fac44f2502d
NicoGim <n.gimenez66@gmail.com>
2019-12-30 16:33:48 +0100

    Remove height for seo

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -90,7 +90,6 @@ class SEO extends Component {
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={image} />
-        <meta property="og:image:height" content="300" />
         <meta
           property="fb:app_id"
           content={config.siteFBAppID ? config.siteFBAppID : ""}
