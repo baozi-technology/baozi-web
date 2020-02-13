6e9a7c919a6f7d068a0c6fd77668737ede186f1e
NicoGim <nicolas@localhost.localdomain>
2020-02-13 21:54:02 +0100

    Remove Commento

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -13,78 +13,75 @@ import ArticleContent from "../components/ArticleContent/ArticleContent"
 import styles from "./post.module.scss"
 import Commento from "../components/Commento/Commento"
 
-export default class PostTemplate extends React.Component {
-  render() {
-    const { data, pageContext } = this.props;
-    const { slug, nexttitle, nextslug, prevtitle, prevslug } = pageContext;
-    const postNode = data.markdownRemark;
-    const id = postNode.id;
-    const tableOfContents = postNode.tableOfContents;
-    const post = postNode.frontmatter;
-    if (!post.id) {
-      post.id = slug;
-    }
-    if (!post.category_id) {
-      post.category_id = config.postDefaultCategoryID;
-    }
-    var articleContent;
-    if(post.cover) {
-      articleContent =
-      (<ArticleContent cover={post.cover.childImageSharp.fluid}/>);
-    } else {
-      articleContent =
-      (<ArticleContent/>);     
-    }
-    const showcaseContent = 
-    (<ShowcaseContent 
-      title={post.title}
-      articleContent={articleContent}/>);
-    return (
-      <Layout showcaseContent={showcaseContent}>
-        <div>
-          <Helmet>
-            <title>{`${post.title} | ${config.siteTitle}`}</title>
-          </Helmet>
-          <SEO postPath={slug} postNode={postNode} postSEO />
-          <hr/>
-          <div className={styles.postPageContainer}>
-            <div className={styles.contentContainer}>
-              {/* <div className={styles.tableOfContentsContainer} dangerouslySetInnerHTML={{__html: tableOfContents}}/> */}
-              <div className={styles.actualContentContainer}>
-                <div className={styles.theArticle} dangerouslySetInnerHTML={{ __html: postNode.html }} />
-              </div>
-            </div>
-            <div className={styles.socialLinks}>
-                {/* <PostTags tags={post.tags} /> */}
-                <SocialLinks postPath={slug} postNode={postNode} />
-            </div>
-            {/* <UserInfo config={config} /> */}
-          </div>
-          <div className={styles.prevNextContainer}>
-            <div className={styles.prevContainer}>
-                <div className={[styles.nextPrevText, styles.maxWdith].join(' ')}>
-                  Previous article:
-                </div>
-                <div className={styles.maxWidth}>
-                  <Link to={nextslug}>
-                    {nexttitle} {/* date reverse order... */}
-                  </Link>
-                </div>
-            </div>
-            <div className={styles.nextContainer}>
-                <div className={[styles.nextPrevText, styles.maxWdith].join(' ')}>
-                  Next article:
-                </div>
-                <div className={styles.maxWidth}>
-                  <Link to={prevslug}>
-                    {prevtitle} {/* date reverse order... */}
-                  </Link>
-                </div>
-            </div>
-          </div>
-          <div className={styles.commentoContainer}>
-            <Commento id={id}/>
-          </div>
+	export default class PostTemplate extends React.Component {
+	  render() {
+	    const { data, pageContext } = this.props;
+	    const { slug, nexttitle, nextslug, prevtitle, prevslug } = pageContext;
+	    const postNode = data.markdownRemark;
+	    const id = postNode.id;
+	    const tableOfContents = postNode.tableOfContents;
+	    const post = postNode.frontmatter;
+	    if (!post.id) {
+	      post.id = slug;
+	    }
+	    if (!post.category_id) {
+	      post.category_id = config.postDefaultCategoryID;
+	    }
+	    var articleContent;
+	    if(post.cover) {
+	      articleContent =
+	      (<ArticleContent cover={post.cover.childImageSharp.fluid}/>);
+	    } else {
+	      articleContent =
+	      (<ArticleContent/>);     
+	    }
+	    const showcaseContent = 
+	    (<ShowcaseContent 
+	      title={post.title}
+	      articleContent={articleContent}/>);
+	    return (
+	      <Layout showcaseContent={showcaseContent}>
+		<div>
+		  <Helmet>
+		    <title>{`${post.title} | ${config.siteTitle}`}</title>
+		  </Helmet>
+		  <SEO postPath={slug} postNode={postNode} postSEO />
+		  <hr/>
+		  <div className={styles.postPageContainer}>
+		    <div className={styles.contentContainer}>
+		      {/* <div className={styles.tableOfContentsContainer} dangerouslySetInnerHTML={{__html: tableOfContents}}/> */}
+		      <div className={styles.actualContentContainer}>
+			<div className={styles.theArticle} dangerouslySetInnerHTML={{ __html: postNode.html }} />
+		      </div>
+		    </div>
+		    <div className={styles.socialLinks}>
+			{/* <PostTags tags={post.tags} /> */}
+			<SocialLinks postPath={slug} postNode={postNode} />
+		    </div>
+		    {/* <UserInfo config={config} /> */}
+		  </div>
+		  <div className={styles.prevNextContainer}>
+		    <div className={styles.prevContainer}>
+			<div className={[styles.nextPrevText, styles.maxWdith].join(' ')}>
+			  Previous article:
+			</div>
+			<div className={styles.maxWidth}>
+			  <Link to={nextslug}>
+			    {nexttitle} {/* date reverse order... */}
+			  </Link>
+			</div>
+		    </div>
+		    <div className={styles.nextContainer}>
+			<div className={[styles.nextPrevText, styles.maxWdith].join(' ')}>
+			  Next article:
+			</div>
+			<div className={styles.maxWidth}>
+			  <Link to={prevslug}>
+			    {prevtitle} {/* date reverse order... */}
+			  </Link>
+			</div>
+		    </div>
+		  </div>
         </div>
       </Layout>
     );
