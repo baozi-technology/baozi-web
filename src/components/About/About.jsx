636505e5ae86152007d378f34e88447859977f7d
NicoGim <nicolas@linux-2.home>
2020-02-21 15:33:34 +0100

    Remove mention of Erlang/OTP in about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,7 +11,7 @@ class About extends Component {
           <p>Most of my professional experiences revolve around softwares written for the physical world industries such as Aerospace and Defense. 
             Only recently did I start doing web development, a very different industry in many ways.</p>
           <p>I always try to code in the language the most suitable for each project, 
-            regardless of my current knowledge and preferences. Disclaimer: I am most experienced in Python and Java, I love Erlang/Elixir/OTP.</p>
+            regardless of my current knowledge and preferences. Disclaimer: I am most experienced in Python and Java but I am rather language-agnostic.</p>
           <p> I recently started{" "} 
             <Link to="/ranch-under-the-hood">contributing to open-source projects</Link> 
             {" "}and I am excited about releasing my own work Soon™.
@@ -33,7 +33,7 @@ class About extends Component {
           <p>Baozi Technology SAS registered in France is my company under which I operate on freelance basis.</p>
           <h2>I can help</h2>
           <p>As of now, I am engaged in a full-time contract with a client. 
-            However, starting from the summer 2020 I will be available remotely or in Paris for Erlang/Elixir/OTP freelance work. If you are interested in working with me, <a title="nicolas.gimenez@baozi.technology" href="mailto:nicolas.gimenez@baozi.technology">shoot me a message!</a></p>
+            However, starting from the summer 2020 I will be available remotely or in Paris for any exciting freelance work. If you are interested in working with me, <a title="nicolas.gimenez@baozi.technology" href="mailto:nicolas.gimenez@baozi.technology">shoot me a message!</a></p>
         </div>
       </div>
     );
